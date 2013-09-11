f0fe3e72efa2b4e908fa8e5acc4a42dc91e46add
fiyarburst <mythic.boost@gmail.com>
2013-09-11 16:54:40 -0400

    the tiniest commits tho

--- threadbot.py
+++ threadbot.py
@@ -34,46 +34,29 @@ sort_by_new = False
 # 1 / Tuesday / How do I make this sound thread
 # 2 / Wednesday / There are no stupid questions thread
 # 3 / Thursday / Marketplace thread
+dayname = "waffles"
 if day == 0:
-    title = config.get("monday", "title") + ' (' + d.strftime("%B %d") + ')'
-    text = config.get("monday", "text")
-    thread_call = {'api_type': 'json', 'kind': 'self', 'sr':sr, 'uh': mh, \
-      'title':  title, 'text': text }
+    dayname = "monday"
     sort_by_new = True
 elif day == 1:
-    title =  config.get("tuesday", "title") + ' (' + d.strftime("%B %d") + ')'
-    text = config.get("tuesday", "text")
-    thread_call = {'api_type': 'json', 'kind': 'self', 'sr':sr, 'uh': mh, \
-      'title':  title, 'text': text }
+    dayname = "tuesday"
     sort_by_new = True
 elif day == 2:
-    thread_call = {'api_type': 'json', 'kind': 'self', 'sr':sr, 'uh': mh, \
-     'title': '"No Stupid Questions" Thread (' + d.strftime("%B %d") + ")", \
-     'text': "While you should search, read the Newbie FAQ, and " + \
-      "definitely [RTFM](http://en.wikipedia.org/wiki/RTFM) when you have a question, some days " + \
-      "you just [can't get rid of a bomb](http://cdn.uproxx.com/wp-content/uploads/2011/08/tumblr_lpnoa80qJS1qj4b9to2_r1_500.gif)." + \
-      " Ask your ~~stupid~~ questions here." }
+    dayname = "wednesday"
     sort_by_new = True
 elif day == 3:
-    thread_call = {'api_type': 'json', 'kind': 'self', 'sr':sr, 'uh': mh, \
-     'title': 'edmp Marketplace Thread (' + d.strftime("%B %d") + ")", \
-     'text': "This thread is where you may share or request services" + \
-              " you have to offer to the edmproduction community. Post your " + \
-              " programs and plugins, your mastering/teaching/coaching/artwork " + \
-              "services, your website/tutorials, your preset/sample packs, your" + \
-              " labels- anything but actual music itself.\n\n**Rules:**\n\n1. No " + \
-              "posting music. No posting your soundcloud when you're looking for " + \
-              "labels, no ghost production; nothing that constitutes you selling or " + \
-              "sharing your own created tracks.\n\n2. Spam will not be tolerated. Repeated " + \
-              "postings for the same product/service in the same thread will not be allowed, " + \
-              "but you can wait until the following week to repost.\n\n3. Mark very clearly " + \
-              "whether you're requesting or offering services, and if you're offering them, " + \
-              "whether those services are paid or free.\n\nAs with the rest of the subreddit, " + \
-              "final decisions over what constitutes an acceptable posting here will be at the " + \
-              "sole discretion of the mods." }
+    dayname = "thursday"
     sort_by_new = False
 else:
     sys.exit()
+
+title = config.get(dayname, "title") + ' (' + d.strftime("%B %d") + ')'
+text = config.get(dayname, "text")
+thread_call = {'api_type': 'json',
+                 'kind': 'self', 
+                 'sr':sr, 'uh': mh, 
+                 'title':  title, 
+                 'text': text }
 
 #### Post thread, 'r' is the results; thread_r is a dict of r
 
@@ -99,8 +82,6 @@ name = thread_r['name']
 tid = thread_r['id']
 url = thread_r['url'] 
 
-print "herro?"
-
 #### Mod-Distinguish thread
 
 dist_data = {'api_type': 'json', 'how':'yes', 'id':name, 'uh': mh}
@@ -113,7 +94,7 @@ if len(thread_r['errors']) > 0:
 #### Edit to include "sort by new" link
 if sort_by_new:
     url = url + '?sort=new'
-    body_text = "* [Please sort this thread by new!]("+url+")\n\n* " + thread_call['text']
+    body_text = "* [Please sort this thread by new!]("+url+")\n\n " + thread_call['text']
     edit_data = {'api_type': 'json', 'text': body_text, 'thing_id':name, 'uh': mh}
     r = s.post('http://www.reddit.com/api/editusertext', data=edit_data, cookies = cookie)
 
