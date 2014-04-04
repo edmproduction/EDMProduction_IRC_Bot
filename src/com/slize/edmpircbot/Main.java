6e91df871e8c2e958cf5ae49d8e2e6220e3a33d3
SliZeing <slize@slize.net>
2014-04-04 20:25:51 +0200

    Added a try catch statement to Main so that the bot doesn't crash with an exception when no config file is supplied.

--- src/com/slize/edmpircbot/Main.java
+++ src/com/slize/edmpircbot/Main.java
@@ -6,9 +6,19 @@ public class Main {
         String[] botCfg;
         String[] nickservCfg;
         String[] redditCfg;
+        Config config;
         Bot bot;
 
-        Config config = new Config(args[0]);
+        // If the user does not supply a config, then give the them a warning and end the program.
+        try {
+            config = new Config(args[0]);
+        }
+        catch(ArrayIndexOutOfBoundsException err) {
+            System.err.println("A config is needed.\n" + "java -jar <JarName> <Config>");
+            config = null;
+
+            System.exit(0);
+        }
 
         botCfg = config.loadBotSettings();
         nickservCfg = config.loadNickServ();
